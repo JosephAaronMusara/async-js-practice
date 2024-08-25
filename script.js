e3a9f44a8f3dcb8265f9207f73ffad93e43bcc77
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-08-25 17:35:07 +0200

    async and await

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-//fetch api
-fetch('todos/second.json').then(response =>{
-    console.log('resolved : ', response);
+//async and await
+
+const getTodos = async () => {
+    const response = await fetch('todos/first.json');
     return response.json();
-}).then(data=>{
-    console.log('the actual data : ',data);
-}).catch(err =>{
-    console.log('rejected :', err);
-});
+}
+
+getTodos()
+    .then(data=>console.log('resolved : ', data));
+
+// //fetch api
+// fetch('todos/second.json').then(response =>{
+//     console.log('resolved : ', response);
+//     return response.json();
+// }).then(data=>{
+//     console.log('the actual data : ',data);
+// }).catch(err =>{
+//     console.log('rejected :', err);
+// });
