80129389cc15e3057cb3847bde5d9bf6c930b00c
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-08-25 16:26:49 +0200

    callback hell, with local json

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const request = new XMLHttpRequest();
+const getTodos = (resource,callback) =>{
+    const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange',()=>{
-    //console.log(request,request.readyState);
-    if(request.readyState === 4 && request.status===200){
-        console.log(request,request.responseText);
-    }
-});
+    request.addEventListener('readystatechange',()=>{
+        if(request.readyState === 4 && request.status===200){
+            const data = JSON.parse(request.responseText);
+            callback(undefined,data);
+        } else if(request.readyState===4){
+            callback(`finished but could not fetch data. Status ${request.status}!!!`,undefined);
+        }
+    });
 
-request.open('GET','https://jsonplaceholder.typicode.com/todos');
-request.send();
+    request.open('GET',resource);
+    request.send();
+};
+
+getTodos('todos/first.json',(err,data)=>{
+        console.log(data);
+        getTodos('todos/second.json',(err,data) =>{
+            console.log(data);
+            getTodos('todos/third.json',(err,data) => {
+                console.log(data);
+                //this act of nesting callbacks is called callback hell and is a bad practice
+            });
+        });
+    });
