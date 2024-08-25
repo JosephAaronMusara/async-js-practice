27dc83dc1a254f2dbf6365e85824595154ba5ad9
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-08-25 15:12:58 +0200

    readystatechange

---
+++ script.js
@@ -0,0 +1,11 @@
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange',()=>{
+    //console.log(request,request.readyState);
+    if(request.readyState === 4 && request.status===200){
+        console.log(request,request.responseText);
+    }
+});
+
+request.open('GET','https://jsonplaceholder.typicode.com/todos');
+request.send();
