01e8d99f5a9f3981f1399656f76c985b91ec6e44
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-08-25 17:46:20 +0200

    async and await, custom errors

--- script.js
+++ script.js
@@ -2,11 +2,16 @@
 
 const getTodos = async () => {
     const response = await fetch('todos/first.json');
-    return response.json();
+    if(response.status !== 200){
+        throw new Error('Failed to fetch data from resource');
+    }
+    const data =await response.json();
+    return data;
 }
 
 getTodos()
-    .then(data=>console.log('resolved : ', data));
+    .then(data => console.log('resolved : ', data))
+    .catch(err => console.log('rejected : ',err.message));
 
 // //fetch api
 // fetch('todos/second.json').then(response =>{
